5837691867f94a7093abffbbd0405819ffab280e
Mehmet Eren Gümüş <yunhanref@proton.me>
2026-09-18 12:31:22 +0300

    Update ss.py

--- ss.py
+++ ss.py
@@ -4,14 +4,12 @@ import time
 def screenshot():
     with mss.mss() as sct:
         pygame_window = {
-            "top":190,
-            "left":320,
-            "width": 1280,
-            "height": 688 
+            "top":360,
+            "left":640,
+            "width": 640,
+            "height": 360 
             }
         os.makedirs("imgs", exist_ok=True)
-        
         sct_img = sct.grab(pygame_window)
         mss.tools.to_png(sct_img.rgb, sct_img.size, output="imgs/pygame_check.png")
-        
         time.sleep(2)
