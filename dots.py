23cfe19aa885927f103053b9d106897e3214a76a
Mehmet Eren Gümüş <yunhanref@proton.me>
2026-09-19 16:21:10 +0300

    Update dots.py

--- dots.py
+++ dots.py
@@ -12,7 +12,6 @@ screen = pygame.display.set_mode((640, 360))
 clock = pygame.time.Clock()
 running = True
 
-# Zamanı takip etmek için bir değişken (milisaniye cinsinden)
 son_degisim_zamanı = 0
 koordinatlar = []
 
@@ -21,28 +20,26 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # Şu anki zamanı al (milisaniye)
     simdiki_zaman = pygame.time.get_ticks()
 
-    # Eğer son değişim üzerinden 1000 milisaniye (2 saniye) geçtiyse yeni koordinat üret
     if simdiki_zaman - son_degisim_zamanı > 2000:
         koordinatlar = [
-            (random.randint(0, 640), random.randint(0, 360)), # Kırmızı top için
-            (random.randint(0, 640), random.randint(0, 360)), # Yeşil top için
-            (random.randint(0, 640), random.randint(0, 360))  # Mavi top için
+            (random.randint(0, 640), random.randint(0, 360)), # Kırmızı
+            (random.randint(0, 640), random.randint(0, 360)), # Yeşil
+            (random.randint(0, 640), random.randint(0, 360))  # Mavi
         ]
-        son_degisim_zamanı = simdiki_zaman # Zamanı güncelle
+        son_degisim_zamanı = simdiki_zaman 
 
-    # Ekranı temizle (önceki kareyi sil)
+  
     screen.fill((0, 0, 0))
 
-    # Eğer koordinatlar belirlendiyse topları çiz
+
     if koordinatlar:
         pygame.draw.circle(screen, (255, 0, 0), koordinatlar[0], 25)
         pygame.draw.circle(screen, (0, 255, 0), koordinatlar[1], 25)
         pygame.draw.circle(screen, (0, 0, 255), koordinatlar[2], 25)
 
-    # Ekranı güncelle
+
     pygame.display.flip()
     screenshot()
     clock.tick(60)
